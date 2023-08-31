0085853016eb1674726478b4779aed09f373bd01
Darwin Mazano <manza_92@yahoo.com>
2023-08-30 21:07:31 -0600

    agregando const

--- condicionales.js
+++ condicionales.js
@@ -1,4 +1,4 @@
-const nombre = "ada school";
+/*const nombre = "ada school";
 let edad = 123;
 let nota = 7;
 if (nombre === "ada school" && nota > 7) {
@@ -6,3 +6,41 @@ console.log("Hola " + nombre + " su nota es = " + nota);
 }else{
     console.log("el nombre no existe")
 }
+const Positivo = "positivo";
+const Negativo = "negativo";
+
+console.log(Positivo);
+console.log(Negativo);
+
+let numeroPositivo = 1;
+let numeroNegativo = -1;
+
+if (numeroPositivo > 0) {
+    console.log("El número positivo es mayor que cero.");
+} else if (numeroPositivo < 0) {
+    console.log("El número positivo es menor que cero.");
+} else {
+    console.log("El número positivo es cero.");
+}
+
+if (numeroNegativo > 0) {
+    console.log("El número negativo es mayor que cero.");
+} else if (numeroNegativo < 0) {
+    console.log("El número negativo es menor que cero.");
+} else {
+    console.log("El número negativo es cero.");
+}*/
+
+function areaCircunferencia (radio) {
+    const pi = 3.1415169;
+    const resultado =pi * radio * radio;
+    return resultado;
+}
+const result = areaCircunferencia(324)
+const result1 = areaCircunferencia(143)
+const result2 = areaCircunferencia(567)
+const result3 = areaCircunferencia(785)
+console.log(result)
+console.log(result1)
+console.log(result2)
+console.log(result3)
