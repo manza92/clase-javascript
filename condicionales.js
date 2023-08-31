db155ecd81b948a07b4be65eda07fa7fe03d0183
Darwin Mazano <manza_92@yahoo.com>
2023-08-30 20:08:04 -0600

    proyecto de javascrip

---
+++ condicionales.js
@@ -0,0 +1,8 @@
+const nombre = "ada school";
+let edad = 123;
+let nota = 7;
+if (nombre === "ada school" && nota > 7) {
+console.log("Hola " + nombre + " su nota es = " + nota);
+}else{
+    console.log("el nombre no existe")
+}
