e1ec936a276d7643e4dd314295b72e2379e5765e
Darwin Mazano <manza_92@yahoo.com>
2023-09-01 20:53:43 -0600

    agreagand arrays

---
+++ arrays.js
@@ -0,0 +1,16 @@
+/*let frutas = [];
+
+frutas[0] = "Pera";
+frutas[1] = "Manzana";
+frutas[2] = "Fresa";
+frutas[2] = "Fresa";
+frutas[2] = "Fresa";
+frutas[2] = "Fresa";
+
+frutas[0] = "Perilla"
+
+console.log(frutas);*/
+
+let miFamilia = ["Juan", "María", "Pedro", "Luna (mascota)"];
+
+console.log(miFamilia);
